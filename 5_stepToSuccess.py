c7fa1a46449f3b2c520d16d60cf681f8ceb082f1
J AJITESH <jalluri3210@gmail.com>
2021-05-31 21:49:51 +0530

    files added
    used python to make this project.

---
+++ 5_stepToSuccess.py
@@ -0,0 +1,41 @@
+print("5 steps for a Successful project")
+print()
+print("A project made by J Ajitesh")
+print()
+
+step = input("Type the Step number(0 to 5):- ")
+print()
+
+if(step == '1'):
+    print("Step 1: Starting")
+    print("Every project starts somewhere. Decide the theme for your project")
+    print("Re-run the project in your terminal to start again.")
+    print()
+
+elif(step == '2'):
+    print("Step 2: Negotiation.")
+    print("Once you've got your ideas, you need to make sure they are acceptable to your intended audience (usually your teacher)")
+    print("Re-run the project in your terminal to start again.")
+    print()
+
+elif(step == '3'):
+    print("Step 3: Time and Resources")
+    print("By this point, you (and your group) will probably be working with only the lightest supervision, so you really need to pay attention to what you actually need to do, and when you need to do it by.")
+    print("Re-run the project in your terminal to start again.")
+    print()
+
+elif(step == '4'):
+    print("Step 4: Outcome")
+    print("After all your work, you will, eventually, finish your project, hopefully to the pleased surprise of your teacher. Take a moment to be proud of yourself and your colleagues.")
+    print("Re-run the project in your terminal to start again.")
+    print()
+
+elif(step == '5'):
+    print("Step 5: Judgement")
+    print(" As the philosopher says, failure is always an option. But, so is success. Once you have presented your final piece of work, it will be judged. Hopefully, you will have succeeded")
+    print("Re-run the project in your terminal to start again.")
+    print()
+
+if(step >= '6'):
+    print("Sorry! There are only 5 steps.")
+    print()
